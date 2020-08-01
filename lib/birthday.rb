04c6b200b6d28b66ea1c58844bad67f6a0d081ce
George Talnagi <gtalnagi@yahoo.com>
2020-08-01 23:34:01 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,9 +13,9 @@ end
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.collect do |kids_name, age|
     if age <= 12 
-      return “Happy Birthday #{kids_name}! You are now #{age} years old!”
+      print “Happy Birthday #{kids_name}! You are now #{age} years old!”
     else 
-      return "You are too old for this."
+      print "You are too old for this."
   end
  end 
  end 
